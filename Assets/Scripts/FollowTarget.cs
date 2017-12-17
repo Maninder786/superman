107034dd1b9d63f9a17ea43c10bb05c6c8a9196d
Maninder786 <Maninder786@users.noreply.github.com>
2017-12-17 09:38:06 -0500

    Superman

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField]
+    Transform mTarget;
+    [SerializeField]
+    float mFollowSpeed;
+    [SerializeField]
+    float mFollowRange;
+
+    
+    public float dist;
+
+    float mArriveThreshold = 0.05f;
+
+    void Update()
+    {
+        if (mTarget != null)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, mTarget.transform.position, mFollowSpeed);
+
+
+        }
+    }
+
+    public void SetTarget(Transform target)
+    {
+        mTarget = target;
+    }
+}
---
+++ Assets/Scripts/Jamminger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jamminger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject mExplosionPrefab;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // TODO: Check if I am being hit by a bullet
+        //          - Destroy the bullet
+        //          - Destroy myself
+        //          - Instantiate an explosion (use the prefab "mExplosionPrefab")
+        if (col.gameObject.layer == LayerMask.NameToLayer("BusterBullet")) 
+        {
+            Instantiate(mExplosionPrefab, transform.position, Quaternion.identity);
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        // TODO: Check if it's Mega Man (ignore everything else)
+        //       If it is Mega Man, make him take some damage (e.g. 3 points)
+        //       Use the TakeDamage() function from the MegaMan class!
+        //       Check DeathZone.cs for an example of what to do !~
+        if(col.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            col.GetComponent<MegaMan>().TakeDamage(1);
+        }
+    }
+}
